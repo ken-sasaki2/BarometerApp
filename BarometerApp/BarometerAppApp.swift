1a65fc0193c8c3e183fb02d1b98a46eb43e93886
佐々木　謙 <nkekisasa222@gmail.com>
2022-04-28 15:24:56 +0900

    Initial Commit

---
+++ BarometerApp/BarometerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarometerAppApp.swift
+//  BarometerApp
+//
+//  Created by sasaki.ken on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct BarometerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarometerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarometerApp
+//
+//  Created by sasaki.ken on 2022/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
